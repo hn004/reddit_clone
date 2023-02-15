b5c5ea8691dd5470614921eb262907662d0d7a3e
Harshal <harshalnikam60.com>
2023-02-15 11:42:56 +0530

    created required entities

---
+++ reddit/src/main/java/com/hn004/reddit/Entity/Subreddit.java
@@ -0,0 +1,40 @@
+ package com.hn004.reddit.Entity;
+
+import java.time.Instant;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Subreddit {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank(message = "name is required")
+	private String name;
+	@NotBlank(message = "description is required")
+	private String description;
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<Post> posts;
+	private Instant createDate;
+	@ManyToOne(fetch=FetchType.LAZY)
+	private User user;
+
+}
---
+++ reddit/src/main/java/com/hn004/reddit/Entity/VoteType.java
@@ -0,0 +1,11 @@
+package com.hn004.reddit.Entity;
+
+public enum VoteType {
+	
+	UPVoTE(1),DOWNVOTE(-1),
+	;
+	VoteType(int direction){
+		
+	}
+
+}
