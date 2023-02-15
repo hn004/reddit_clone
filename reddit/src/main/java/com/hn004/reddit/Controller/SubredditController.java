1e1504836d6b0f4203b673e0581768b62e0c55d5
Harshal <harshalnikam60.com>
2023-02-15 19:37:13 +0530

    created method to fetch subreddit by id

--- reddit/src/main/java/com/hn004/reddit/Controller/SubredditController.java
+++ reddit/src/main/java/com/hn004/reddit/Controller/SubredditController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,11 @@ public class SubredditController {
 	@GetMapping
 	public ResponseEntity<List<SubredditDto>> getAllSubreddit() {
 		return ResponseEntity.status(HttpStatus.OK).body(subredditService.getAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<SubredditDto> getSubreddit(@PathVariable Long id ){
+		return new ResponseEntity<SubredditDto>(subredditService.getSubreddit(id),HttpStatus.OK);
 	}
 
 }
