558a8ca16e5308ca1da0a74ca373f52834c8fcad
Harshal <harshalnikam60.com>
2023-02-19 14:52:46 +0530

    initial commit

--- reddit/src/main/java/com/hn004/reddit/Service/MailContentBuilder.java
+++ reddit/src/main/java/com/hn004/reddit/Service/MailContentBuilder.java
@@ -5,17 +5,25 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.hn004.reddit.Entity.NotificationEmail;
+import com.hn004.reddit.Entity.User;
+
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class MailContentBuilder {
-	
+
 	@Autowired
 	private TemplateEngine templateEngine;
-	
+
+
 	String build(String message) {
-		Context context=new Context();
+		Context context = new Context();
 		context.setVariable("message", message);
 		return templateEngine.process("mailTemplate", context);
 	}
+
 	
 
 }
