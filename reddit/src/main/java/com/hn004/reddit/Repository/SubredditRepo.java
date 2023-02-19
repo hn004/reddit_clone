0259d006b9a3590c8af28229cc0bc1b3f18bf461
Harshal <harshalnikam60.com>
2023-02-19 14:52:26 +0530

    Repository for subreddits

--- reddit/src/main/java/com/hn004/reddit/Repository/SubredditRepo.java
+++ reddit/src/main/java/com/hn004/reddit/Repository/SubredditRepo.java
@@ -1,5 +1,7 @@
 package com.hn004.reddit.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.hn004.reddit.Entity.Subreddit;
 
 @Repository
 public interface SubredditRepo extends JpaRepository<Subreddit, Long>{
+
+//	Optional<Subreddit> findByName(String subredditName);
+	Optional<Subreddit> findBySubredditName(String subredditName);
 
 }
