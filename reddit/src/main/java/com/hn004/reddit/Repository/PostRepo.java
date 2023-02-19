fe3418ceb176899794adeb72f350a1c1d937bd36
Harshal <harshalnikam60.com>
2023-02-19 14:53:32 +0530

    Repository for the posts

--- reddit/src/main/java/com/hn004/reddit/Repository/PostRepo.java
+++ reddit/src/main/java/com/hn004/reddit/Repository/PostRepo.java
@@ -1,11 +1,21 @@
 package com.hn004.reddit.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hn004.reddit.Entity.Post;
+import com.hn004.reddit.Entity.Subreddit;
+import com.hn004.reddit.Entity.User;
+import com.hn004.reddit.dto.PostResponse;
 
 @Repository
 public interface PostRepo extends JpaRepository<Post, Long> {
+
+	List<Post> findAllBySubreddit(Subreddit subreddit);
+
+	List<PostResponse> findByUser(User user);
 
 }
