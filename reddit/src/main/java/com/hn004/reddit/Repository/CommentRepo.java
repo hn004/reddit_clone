62c1fe4207d307fc01bd2f96dc9de6a1b598ec4b
Harshal <harshalnikam60.com>
2023-02-19 14:52:04 +0530

    added findbyusername method

--- reddit/src/main/java/com/hn004/reddit/Repository/CommentRepo.java
+++ reddit/src/main/java/com/hn004/reddit/Repository/CommentRepo.java
@@ -1,9 +1,15 @@
 package com.hn004.reddit.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hn004.reddit.Entity.Comment;
+import com.hn004.reddit.Entity.Post;
+import com.hn004.reddit.Entity.User;
 
 public interface CommentRepo extends JpaRepository<Comment, Long>{
-
+ 
+	List<Comment> findByPost(Post post);
+	List<Comment> findAllByUser(User user);
 }
