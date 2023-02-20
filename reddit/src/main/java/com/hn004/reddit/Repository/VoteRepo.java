482146191147a19bdb93046194d132be5aebadc6
Harshal <harshalnikam60.com>
2023-02-20 10:32:57 +0530

    findTopByPostAndUserOrderByVoteIdDesc method added

--- reddit/src/main/java/com/hn004/reddit/Repository/VoteRepo.java
+++ reddit/src/main/java/com/hn004/reddit/Repository/VoteRepo.java
@@ -1,11 +1,15 @@
 package com.hn004.reddit.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hn004.reddit.Entity.Post;
+import com.hn004.reddit.Entity.User;
 import com.hn004.reddit.Entity.Vote;
 
 @Repository
 public interface VoteRepo extends JpaRepository<Vote, Long> {
-
+     Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post,User currentUser);
 }
